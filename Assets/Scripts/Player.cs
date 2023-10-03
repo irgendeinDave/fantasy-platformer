17c2823d14a21396563bbf536d01ca567e8590e4
david <zollner.david010306@gmail.com>
2023-10-03 15:56:51 +0200

    add basic file structure and implement basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float MoveForce = 10f;
+    [SerializeField] private float MaxMoveSpeed = 15f;
+    
+    [SerializeField] private float JumpForce = 50f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.angularVelocity = 0;
+        
+        if ((rb.velocity.x < MaxMoveSpeed && Input.GetAxis("Horizontal") > 0) || 
+            (rb.velocity.x > -MaxMoveSpeed && Input.GetAxis("Horizontal") < 0))
+        {
+            rb.AddForce(Vector2.right * (MoveForce * Input.GetAxis("Horizontal")));
+            // show animation
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * JumpForce);
+            // show animation
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Rarities.cs
@@ -0,0 +1,9 @@
+
+public enum Rarities
+{ 
+    Common,
+    Rare,
+    Epic,
+    Mystical,
+    Lengendary,
+}
